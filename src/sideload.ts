df112030bc763d23b360110c9ab077a393205fdd
Rohit Reddy Abbadi <tiholic@users.noreply.github.com>
2022-07-16 16:39:58 +0530

    create 2 entry points
    - one for sideloading into UI (src/sideload.ts)
    - another to be used as a package (src/index.ts)

--- src/sideload.ts
+++ src/sideload.ts
@@ -1,14 +1,19 @@
 import Docupilot from './docupilot';
+
 import * as _API from './api';
 
 class _Docupilot extends Docupilot {
+  readonly AuthTokensService = _API.AuthTokensService;
   readonly FoldersService = _API.FoldersService;
-  readonly TemplatesService = _API.TemplatesService;
+  readonly GeneralService = _API.GeneralService;
+  readonly GenerateBulkService = _API.GenerateBulkService;
   readonly GenerateService = _API.GenerateService;
   readonly HistoryService = _API.HistoryService;
   readonly LinkedAccountsService = _API.LinkedAccountsService;
-  readonly GenerateBulkService = _API.GenerateBulkService;
+  readonly PersonalizationService = _API.PersonalizationService;
+  readonly SubscriptionService = _API.SubscriptionService;
   readonly TemplateDeliveryService = _API.TemplateDeliveryService;
+  readonly TemplatesService = _API.TemplatesService;
   readonly UsersService = _API.UsersService;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-  entry: './src/index.ts',
+  entry: './src/sideload.ts',
   target: ['web', 'es5'],
   module: {
     rules: [
