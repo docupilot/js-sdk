d3d06d50b729575672ce50001b3a4036c81f2889
Rohit Reddy Abbadi <tiholic@users.noreply.github.com>
2022-09-01 11:54:20 +0530

    add DeliveryAccount.used_by

--- src/api/models/DeliveryAccount.ts
+++ src/api/models/DeliveryAccount.ts
@@ -6,6 +6,7 @@ export type DeliveryAccount = {
     readonly id: number;
     type: 'hellosign' | 'aws_s3';
     readonly sub_type: string;
+    readonly used_by: number;
     readonly name: string | null;
     credentials: Record<string, any>;
     readonly is_expired: boolean;
