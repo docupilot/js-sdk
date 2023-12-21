bd0c308f6d026af49eb839da78c202ae454af727
ravalikab <ravalikab@users.noreply.github.com>
2023-12-21 07:32:09 +0000

    [create-pull-request] automated change

--- src/api/models/DocuSignDelivery.ts
+++ src/api/models/DocuSignDelivery.ts
@@ -6,13 +6,22 @@ export type DocuSignDelivery = {
     readonly id: number;
     account: number;
     custom_tags?: Record<string, Record<string, Array<any>>> | null;
+    signers: Array<{
+        order?: string;
+        recipient_type: 'signer' | 'agent' | 'cc' | 'editor';
+        name: string;
+        is_email_delivery: boolean;
+        is_sms_delivery: boolean;
+        email_address?: string;
+        country_code?: string;
+        phone_number?: string;
+    }>;
     name: string;
     type?: 'docu_sign';
     success_callback_url?: string | null;
     failure_email_recipients?: string | null;
     subject?: string | null;
     message?: string | null;
-    signers: Record<string, any>;
     fullname_anchor_tags?: boolean;
     sequenced_signing?: boolean;
     expire_after?: number | null;
