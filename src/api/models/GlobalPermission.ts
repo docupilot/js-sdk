a2fa2ae83ee347678cc9b1497d9f2d67975e612d
ravalikab <ravalikab@users.noreply.github.com>
2023-12-22 05:37:38 +0000

    [create-pull-request] automated change

---
+++ src/api/models/GlobalPermission.ts
@@ -0,0 +1,17 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type GlobalPermission = {
+    templates: boolean;
+    folders: boolean;
+    workspace: boolean;
+    'workspace:update': boolean;
+    'workspace:delete': boolean;
+    billing: boolean;
+    'team:manage': boolean;
+    integrations: boolean;
+    reports: boolean;
+    api_settings: boolean;
+};
+
--- src/api/services/PermissionsService.ts
+++ src/api/services/PermissionsService.ts
@@ -1,6 +1,8 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { GlobalPermission } from '../models/GlobalPermission';
+
 import type { CancelablePromise } from '../core/CancelablePromise';
 import { OpenAPI } from '../core/OpenAPI';
 import { request as __request } from '../core/request';
@@ -9,14 +11,52 @@ import { OmitReadonly } from '../core/utils/OmitReadonly';
 export class PermissionsService {
 
     /**
-     * Returns permission for each role
-     * @returns any No response body
+     * Returns folder permissions
+     * @returns any
      * @throws ApiError
      */
-    public static getGlobalPermissions(): CancelablePromise<any> {
+    public static getFolderPermissions({
+        ids,
+    }: {
+        ids?: string,
+    }): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/v2/permissions/folder/',
+            query: {
+                'ids': ids,
+            },
+        });
+    }
+
+    /**
+     * Returns permission for each role
+     * @returns GlobalPermission
+     * @throws ApiError
+     */
+    public static getGlobalPermissions(): CancelablePromise<GlobalPermission> {
         return __request(OpenAPI, {
             method: 'GET',
             url: '/api/v2/permissions/global/',
+        });
+    }
+
+    /**
+     * Returns template permissions
+     * @returns any
+     * @throws ApiError
+     */
+    public static getTemplatePermissions({
+        ids,
+    }: {
+        ids?: string,
+    }): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/v2/permissions/template/',
+            query: {
+                'ids': ids,
+            },
         });
     }
 
