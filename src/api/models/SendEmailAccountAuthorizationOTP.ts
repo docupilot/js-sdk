ce13d38bf6d24d40da491d8b1407caf584c285a7
deekshith1856 <deekshith1856@users.noreply.github.com>
2024-05-02 09:53:35 +0000

    [create-pull-request] automated change

--- src/api/models/SendEmailAccountAuthorizationOTP.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type SendEmailAccountAuthorizationOTP = {
-    action_name: 'verify' | 'update';
-    email?: string | null;
-};
-
