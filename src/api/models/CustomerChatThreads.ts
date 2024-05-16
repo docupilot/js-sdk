40cbc6ee14346da686145b86efcecf9df8dbdce5
SumanthDocupliot <SumanthDocupliot@users.noreply.github.com>
2024-05-16 17:32:12 +0000

    [create-pull-request] automated change

---
+++ src/api/models/CustomerChatThreads.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type CustomerChatThreads = {
+    readonly id: number;
+    thread_id: string;
+    thread_name?: string | null;
+    user: number;
+};
+
