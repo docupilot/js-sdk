02509a21e558a46586104654b62f9ccc99ff01c5
ravalikab <ravalikab@users.noreply.github.com>
2024-06-17 08:40:57 +0000

    [create-pull-request] automated change

---
+++ src/api/models/ChatThreads.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ChatThreads = {
+    readonly id: number;
+    thread_id: string;
+    thread_name?: string | null;
+};
+
