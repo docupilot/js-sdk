24c7c13cec5346074fb60bba527175b6956be65c
ravalikab <ravalikab@users.noreply.github.com>
2024-06-28 09:20:50 +0000

    [create-pull-request] automated change

--- src/api/index.ts
+++ src/api/index.ts
@@ -52,6 +52,7 @@ export type { Invitation } from './models/Invitation';
 export type { MergeHistory } from './models/MergeHistory';
 export type { Meta } from './models/Meta';
 export type { MoveTemplate } from './models/MoveTemplate';
+export type { Nested } from './models/Nested';
 export type { NewTemplate } from './models/NewTemplate';
 export type { NotFoundError } from './models/NotFoundError';
 export type { OneDriveDelivery } from './models/OneDriveDelivery';
