3d9f2b999c268e45a4cbeba4746ce627961bc7af
ravalikab <ravalikab@users.noreply.github.com>
2024-07-03 12:55:09 +0000

    [create-pull-request] automated change

---
+++ src/api/models/GalleryCategory.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type GalleryCategory = {
+    readonly id: number;
+    name: string;
+};
+
--- src/api/models/TemplateGallery.ts
+++ src/api/models/TemplateGallery.ts
@@ -2,12 +2,14 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { GalleryCategory } from './GalleryCategory';
+
 export type TemplateGallery = {
     readonly id: number;
     readonly image_path: string;
     readonly file_type: string;
+    category: GalleryCategory;
     title: string;
     description?: string | null;
-    category: 'legal' | 'real_state';
 };
 
