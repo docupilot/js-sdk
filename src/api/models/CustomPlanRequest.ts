94aba4b79e5630b8a1f9660b9d2dab9a888c3746
ravalikab <ravalikab@users.noreply.github.com>
2024-08-21 07:24:00 +0000

    [create-pull-request] automated change

---
+++ src/api/models/CustomPlanRequest.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type CustomPlanRequest = {
+    credits: number;
+    users: number;
+    additional_notes?: string;
+};
+
