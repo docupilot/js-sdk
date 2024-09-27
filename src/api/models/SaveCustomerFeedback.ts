85a2e896191d32edc176f3c0747ce53fbc2923b1
deekshith1856 <deekshith1856@users.noreply.github.com>
2024-09-27 08:16:08 +0000

    [create-pull-request] automated change

---
+++ src/api/models/SaveCustomerFeedback.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type SaveCustomerFeedback = {
+    feedback: {
+        challanges_faced: any[];
+        feature_improvement?: string;
+    };
+};
+
--- src/api/models/SaveUserFeedback.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type SaveUserFeedback = {
-    question_field: string;
-    value: (string | Array<string>);
-};
-
