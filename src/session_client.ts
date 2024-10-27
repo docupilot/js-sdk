641af775a533aa3a793b5dc1ddef7c50baeeffab
deekshith1856 <deekshith@docupilot.app>
2024-10-27 13:38:59 +0530

    Added ai service

--- src/session_client.ts
+++ src/session_client.ts
@@ -24,7 +24,7 @@ export class DocupilotSessionClient extends Docupilot {
   readonly DomainService = _API.DomainService;
   readonly GalleryTemplatesService = _API.GalleryTemplatesService;
   readonly AiChatService = _API.AiChatService;
-
+  readonly AiService = _API.AiService;
   configureHeadersInterceptor(getHeaders: () => Record<string, string>) {
     _API.OpenAPI.HEADERS = async (options: ApiRequestOptions) => {
       const headers = getHeaders();
