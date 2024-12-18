dadc53d9ae46f350b1791a097d0dcffab242dc38
ravalikab <ravalikab@users.noreply.github.com>
2024-12-18 07:08:25 +0000

    [create-pull-request] automated change

--- src/api/models/ChildTemplate.ts
+++ src/api/models/ChildTemplate.ts
@@ -8,7 +8,7 @@ export type ChildTemplate = {
     description?: string | null;
     type?: 'docx' | 'html' | 'fillable_pdf' | 'pptx' | 'xlsx' | 'g_document' | 'g_presentation' | 'g_spreadsheet';
     readonly created_time: string;
-    readonly updated_time: string;
+    readonly updated_time: string | null;
     document_status?: 'active' | 'test';
     deleted_time?: string | null;
     readonly created_by: number;
--- src/api/models/WorkspaceDemographic.ts
+++ src/api/models/WorkspaceDemographic.ts
@@ -5,7 +5,7 @@
 export type WorkspaceDemographic = {
     readonly id: number;
     domain_name: string;
-    employee_count: '1-10' | '11-15' | '51-200' | '201-500' | '501-1000' | '1001-5000' | '5001-10000' | '10000+';
+    employee_count: 'JUST ME' | '2 to 5' | '6 to 10' | '11 to 25' | '26 to 50' | '51 to 200' | '201 to 1,000' | '1,001 to 10,000' | '10,001 or more';
     industry: string;
     primary_region: string;
 };
