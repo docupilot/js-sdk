d8694787f58c4485cf42fa35e3cd3a3b2adcc379
ravalikab <ravalikab@users.noreply.github.com>
2025-01-21 07:20:13 +0000

    [create-pull-request] automated change

---
+++ src/api/models/DocumentMergeHistory.ts
@@ -0,0 +1,17 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ChildDocumentMergeHistory } from './ChildDocumentMergeHistory';
+import type { ChildTemplate } from './ChildTemplate';
+
+export type DocumentMergeHistory = {
+    readonly id: number;
+    data?: Record<string, any> | null;
+    readonly template: ChildTemplate | null;
+    executed_deliveries: Array<ChildDocumentMergeHistory>;
+    readonly is_data_expired: boolean;
+    readonly is_document_expired: boolean;
+    created_file_name?: string | null;
+};
+
