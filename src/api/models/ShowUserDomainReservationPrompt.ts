ecb87437ee3b8c9b2946fc4b036c6b35f13419bb
deekshith1856 <deekshith1856@users.noreply.github.com>
2025-02-03 10:25:45 +0000

    [create-pull-request] automated change

---
+++ src/api/models/ShowUserDomainReservationPrompt.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ShowUserDomainReservationPrompt = {
+    show_prompt: boolean;
+    suggested_domain_name?: string;
+};
+
