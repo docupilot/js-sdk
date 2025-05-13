325af6cbdb47eb49524ad08822fb2718abaff2b4
deekshith1856 <deekshith1856@users.noreply.github.com>
2025-05-13 13:43:32 +0000

    [create-pull-request] automated change

---
+++ src/api/models/BoxDriveDelivery.ts
@@ -0,0 +1,18 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type BoxDriveDelivery = {
+    readonly id: number;
+    failure_email_recipients?: string | null;
+    account: number;
+    name: string;
+    type?: 'box_drive';
+    success_callback_url?: string | null;
+    success_callback_payload?: Record<string, any> | null;
+    success_callback_headers?: Record<string, any> | null;
+    last_used_on?: string | null;
+    folder?: string;
+    folder_id?: string | null;
+};
+
