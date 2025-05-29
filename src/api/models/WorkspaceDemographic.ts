3145e8773dfe987a2452eb7aaf4061fd1c1e3b58
ravalikab <ravalikab@users.noreply.github.com>
2025-05-29 12:35:07 +0000

    [create-pull-request] automated change

--- src/api/models/WorkspaceDemographic.ts
+++ src/api/models/WorkspaceDemographic.ts
@@ -4,7 +4,7 @@
 
 export type WorkspaceDemographic = {
     readonly id: number;
-    domain_name: string;
+    domain_name?: string | null;
     employee_count: 'JUST ME' | '2 to 5' | '6 to 10' | '11 to 25' | '26 to 50' | '51 to 200' | '201 to 1,000' | '1,001 to 10,000' | '10,001 or more';
     industry: string;
     primary_region: string;
