0bec1f2e42ad6ef7170a76aaa014555cc49901c0
ravalikab <ravalikab@users.noreply.github.com>
2025-08-05 12:05:29 +0000

    [create-pull-request] automated change

--- src/api/models/ChildDocumentMergeHistory.ts
+++ src/api/models/ChildDocumentMergeHistory.ts
@@ -4,7 +4,7 @@
 
 export type ChildDocumentMergeHistory = {
     readonly id: number;
-    delivery_type: string;
+    delivery_type: 'webhook' | 'azure_blob_storage' | 'box_drive' | 'signature' | 'docu_sign' | 'sign_now' | 'zoho_crm' | 'dropbox' | 'google_drive' | 'podio' | 'one_drive' | 'eversign' | 'hellosign' | 'aws_s3' | 'signable' | 'yousign' | 'email' | 'sftp';
     status: 'pending' | 'success' | 'error';
     message?: Record<string, any> | null;
     delivery?: number | null;
---
+++ src/api/models/DeliveryCount.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type DeliveryCount = {
+    /**
+     * Number of deliveries linked to the template
+     */
+    delivery_count: number;
+};
+
