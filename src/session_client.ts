336a97b6448a36cffe44e259a404dc906858a5e6
Ravalika Boyapally <ravalika@flackon.com>
2025-08-22 19:40:21 +0530

    Add org service (#351)

--- src/session_client.ts
+++ src/session_client.ts
@@ -27,6 +27,7 @@ export class DocupilotSessionClient extends Docupilot {
   readonly AiService = _API.AiService;
   readonly DemographicService = _API.DemographicService;
   readonly EsignService = _API.EsignService;
+  readonly OrganizationService = _API.OrganizationService;
 
   configureHeadersInterceptor(getHeaders: () => Record<string, string>) {
     _API.OpenAPI.HEADERS = async (options: ApiRequestOptions) => {
