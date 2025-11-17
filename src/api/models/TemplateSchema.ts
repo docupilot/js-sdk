95b15c3350fa3b0dad4e0ccdeddf80ad787c88c3
deekshith1856 <deekshith1856@users.noreply.github.com>
2025-11-17 04:50:52 +0000

    [create-pull-request] automated change

--- src/api/models/TemplateSchema.ts
+++ src/api/models/TemplateSchema.ts
@@ -5,7 +5,7 @@
 export type TemplateSchema = {
     name: string;
     type: 'string' | 'array' | 'object' | 'generic';
-    r_type: 'content_block';
+    rType?: 'content_block';
     generics: 'string' | 'array' | 'object' | 'generic';
     fields: Array<TemplateSchema>;
 };
