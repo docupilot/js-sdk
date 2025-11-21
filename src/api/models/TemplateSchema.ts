0c2b3dbf1e685e3b138800c7e3f931a23b0e520e
ravalikab <ravalikab@users.noreply.github.com>
2025-11-21 09:39:21 +0000

    [create-pull-request] automated change

--- src/api/models/TemplateSchema.ts
+++ src/api/models/TemplateSchema.ts
@@ -5,6 +5,7 @@
 export type TemplateSchema = {
     name: string;
     type: 'string' | 'array' | 'object' | 'generic';
+    rType?: 'content_block';
     generics: 'string' | 'array' | 'object' | 'generic';
     fields: Array<TemplateSchema>;
 };
