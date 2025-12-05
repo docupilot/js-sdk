eb75a5c0bd1ee71767b2e15ff3fb004c00ab2b88
ravalikab <ravalikab@users.noreply.github.com>
2025-12-05 09:55:56 +0000

    [create-pull-request] automated change

---
+++ src/api/models/ESignWebhook.ts
@@ -0,0 +1,17 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ESignWebhook = {
+    readonly id: number;
+    name: string;
+    url: string;
+    active?: boolean;
+    event_subscriptions?: Array<'recipient_viewed' | 'recipient_signed' | 'recipient_declined' | 'envelope_completed' | 'envelope_expired'>;
+    hmac_secret?: string | null;
+    readonly created_at: string;
+    readonly updated_at: string;
+    readonly created_by: number;
+    readonly updated_by: number | null;
+};
+
---
+++ src/api/models/ESignWebhookEventLog.ts
@@ -0,0 +1,18 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ESignWebhookEventLog = {
+    readonly id: number;
+    readonly is_retryable: boolean;
+    event: 'recipient_viewed' | 'recipient_signed' | 'recipient_declined' | 'envelope_completed' | 'envelope_expired';
+    /**
+     * HTTP status code returned by the webhook
+     */
+    status_code: number;
+    readonly last_triggered_at: string;
+    attempts?: number;
+    webhook: number;
+    envelope: string;
+};
+
