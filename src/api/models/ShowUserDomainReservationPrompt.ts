c8921e24ad84f7a34358f2623089f5d61096c60c
ravalikab <25775803+ravalikab@users.noreply.github.com>
2026-02-16 11:46:35 +0000

    [create-pull-request] automated change

--- src/api/models/ShowUserDomainReservationPrompt.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type ShowUserDomainReservationPrompt = {
-    show_prompt: boolean;
-    suggested_domain_name?: string;
-};
-
