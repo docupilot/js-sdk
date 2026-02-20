6a05457f6f1055be9311552489c5fc71e0257348
ravalikab <25775803+ravalikab@users.noreply.github.com>
2026-02-20 05:20:18 +0000

    [create-pull-request] automated change

---
+++ src/accounts/models/SAMLProviderList.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type SAMLProviderList = {
+    readonly id: number;
+    name: string;
+    readonly type: string;
+    readonly is_setup_complete: boolean;
+    active: boolean;
+};
+
