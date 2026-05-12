b0688692a367c9dfbaf9e8fc2316f5532f40e456
ravalikab <25775803+ravalikab@users.noreply.github.com>
2026-05-12 06:16:20 +0000

    [create-pull-request] automated change

--- src/api/models/SharepointDelivery.ts
+++ src/api/models/SharepointDelivery.ts
@@ -12,7 +12,9 @@ export type SharepointDelivery = {
     success_callback_payload?: Record<string, any> | null;
     success_callback_headers?: Record<string, any> | null;
     last_used_on?: string | null;
+    folder_name?: string;
     folder_id: string;
+    drive_name?: string;
     drive_id: string;
 };
 
