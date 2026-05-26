b3d696bb8920f662e624071b180ed9d30731f8f9
ravalikab <25775803+ravalikab@users.noreply.github.com>
2026-05-26 07:21:03 +0000

    [create-pull-request] automated change

---
+++ src/api/models/PatchedESignNotificationSettings.ts
@@ -0,0 +1,16 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type PatchedESignNotificationSettings = {
+    signer_reminder_emails?: boolean;
+    signer_expiry_warning?: boolean;
+    sender_when_signed?: boolean;
+    sender_document_completed?: boolean;
+    sender_when_declined?: boolean;
+    cc_envelope_signed?: boolean;
+    cc_envelope_completed?: boolean;
+    cc_envelope_declined?: boolean;
+    cc_envelope_voided?: boolean;
+};
+
